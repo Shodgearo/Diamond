57e66e6db4ef0cdc0e753367fc96afb9a4f504e1
Shodgearo <Shodkvest@gmail.com>
2018-05-05 20:28:40 +0600

    Tools for window

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 // Game (clone diamond)
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
 
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        
+        initFrame();
+    }
+
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(600, 540));
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
